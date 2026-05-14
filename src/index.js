f9c70750d9d7ec157755c0276f39ef4f6e262f95
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-05-13 19:25:58 -0500

    intigrated AI to JavaScript and targeted poem form in HTML

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-function generatePoem(event) {
-    event.preventDefault();
-
-    let poemElement = document.querySelector("#poem");
-
+function displayPoem(response) {
     new Typewriter("#poem", {
-      strings: "Take me to the moon",
+      strings: response.data.answer,
       autoStart: true,
       delay: 1,
       cursor: "",
     });
+}
+
+
+
+function generatePoem(event) {
+    event.preventDefault();
+    let instructionInput = document.querySelector("#user-instructions");
+    let apiKey = "e2t5dd99b4cfa003ed9aaof63c44d73c";
+    let prompt = `User instructions: Generate a poem about ${instructionInput.value}`;
+    let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+
+    let poemElement = document.querySelector("#poem");
 }
 
 let poemFormElement = document.querySelector("#generator");
